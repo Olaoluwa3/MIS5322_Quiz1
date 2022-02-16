3ee93f6ca87584772924ae31fa341a4abfb43805
Olaoluwa3 <olaoko3@gmail.com>
2022-02-16 14:30:24 -0600

    Completed Quiz 1

--- weaponClass.py
+++ weaponClass.py
@@ -1,4 +1,5 @@
 import random
+from stat import SF_APPEND
 
 '''
 Create a Weapon Class definition according to the following specs:
@@ -12,6 +13,7 @@ range - user supplied
 status - 'Active' (this attribute should be changed to 'Inactive' if bullets 
                     run out.)
 
+
 Methods:
 
 Create accessor methods for each attribute.
@@ -22,6 +24,52 @@ every time the method is called. When the bullet count reaches zero, it should c
 the attribute 'status' to 'Inactive'
 
 '''
+
+class Weapon:
+
+    def __init__(self, name, speed, rangee):
+        self.__name = name
+        self.__speed = speed
+        self.__range = rangee
+        self.__bullets = random.randrange(10,100000)
+        self.__status = 'Active'
+
+    def get_name(self):
+        return self.__name
+
+    def get_speed(self):
+        return self.__speed
+
+    def get_range(self):
+        return self.__range
+    
+    def get_bullets(self):
+        return self.__bullets
+
+    def get_status(self):
+        return self.__status
+
+    
+
+
+    '''
+    Create a method named 'fire_bullet' that will simulate
+firing a bullet. This is accomplished by decreasing the number of bullets by 1 
+every time the method is called. When the bullet count reaches zero, it should change
+the attribute 'status' to 'Inactive'
+    '''   
+
+    def fire_bullet(self):
+        if(self.__bullets > 0):
+            self.__bullets -= 1
+        else:
+            self.__bullets = 0
+            self.__status = 'Inactive'
+            
+
+        
+
+
 
 
 
--- weaponProgram.py
+++ weaponProgram.py
@@ -15,61 +15,74 @@ HINT: Follow the comments for each line to help with the logic of the problem.
 
 
 # create a file object to open the file in read mode
-
+infile = open('weapons.csv', 'r')
 
 
 # create a csv object from the file object
+infile_reader = csv.reader(infile, delimiter = ',')
 
 
 
 #skip the header row
+next(infile_reader)
 
 
 
 
 #create an empty dictionary named 'weapons_dict'
-
+weapons_dict = {}
 
 
 
 #use a for loop to iterate through every row of the csv file
+for record in infile_reader:
 
     #use variables for name,speed and range (optional)
-    
+    name = record[0]
+    speed = record[1]
+    rangee = record[2]
 
     # create an instance of the weapon object using the 
     # specs from the csv file (name,speed and range) 
-    
+    my_weapon = w.Weapon(name, speed, rangee)
 
     # append the name and bullet count to 'weapons_dict'
-    
+    weapons_dict[my_weapon.get_name()] = my_weapon.get_bullets()
 
 
-    # print out the name of the weapon using the appropriate method of the object 
+    # print out the name of the weapon using the appropriate method of the object
+    print('\nWeapon name:', my_weapon.get_name()) 
     
     # print out the speed of the weapon using the appropriate method of the object
+    print('\nWeapon speed:', my_weapon.get_speed())
     
     # print out the range of the weapon using the appropriate method of the object
-    
+    print('\nWeapon range:', my_weapon.get_range())
+
     # print out the number of bullets of the weapon using the appropriate method of the object
-    
+    print('\nNumber of bullets:', my_weapon.get_bullets())
 
     #use an input statement to halt the program and wait for the user - 
     input("Press any key to fire the weapon")
     
 
     # use an appropriate loop to keep firing the weapon until all bullets run out
+    for number in range(my_weapon.get_bullets()):
     
         # call the appropriate method to fire a bullet
+        my_weapon.fire_bullet()
        
         # print out the bullet count every time the weapon is fired
+        print(my_weapon.get_bullets(), end='\r')
         
 
     
 
 
 #using a loop print out the name and number of bullets from the dictionary
-
+for item, value in weapons_dict.items():
+    print("Name:"+ item + " Bullets:" + str(value))
+    
 
 
 
